8b697853759be71f5ebe4abe982e0628fd083d9d
Qunsheng Huang <ga53vuw@tum.de>
2022-08-18 16:26:46 +0200

    Created basic support for python package

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='qib',
+    version='0.0.1',
+    install_requires=[
+        'numpy',
+        'requests',
+        'importlib-metadata; python_version == "3.8"',
+    ],
+)
+
