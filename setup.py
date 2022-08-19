cf568d9d02b6f975d487c591040bfbb3091b1e17
Christian B. Mendl <christian.b.mendl@gmail.com>
2022-08-19 13:51:33 +0200

    update setup.py: requiring scipy

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import setup
 
 setup(
-    name='qib',
-    version='0.0.1',
+    name="qib",
+    version="0.0.1",
     install_requires=[
-        'numpy',
-        'requests',
-        'importlib-metadata; python_version == "3.8"',
+        "numpy",
+        "scipy",
+        "importlib-metadata; python_version == '3.8'",
     ],
 )
-
