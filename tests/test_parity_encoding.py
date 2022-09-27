3c0fd7f7929753b464647144c91bae6fbb9b8162
Martina Nibbi <martinanibbi@gmail.com>
2022-09-27 16:34:50 +0200

    Parity test fixed for real and symmetric matrix

--- tests/test_parity_encoding.py
+++ tests/test_parity_encoding.py
@@ -1,6 +1,10 @@
 import numpy as np
 from scipy import sparse
 import unittest
+
+import sys
+sys.path.append('../src')
+
 import qib
 
 
@@ -13,22 +17,26 @@ class TestParityEncoding(unittest.TestCase):
         # construct a random fermionic field operator with complex coefficients
         latt = qib.lattice.IntegerLattice((2, 3))
         field = qib.field.Field(qib.field.ParticleType.FERMION, latt)
-        # terms with differring number of creation and annihilation operators
+        # only real and symmetric matrix (TODO: extend)
+        coeffs = np.random.standard_normal(2 * (latt.nsites,))                  # double coefficient because it's a double sum
+        # symmetrize
+        coeffs = 0.5 * (coeffs + coeffs.T)
+        term = qib.operator.FieldOperatorTerm(
+            [qib.operator.IFODesc(field, qib.operator.IFOType.FERMI_CREATE),
+             qib.operator.IFODesc(field, qib.operator.IFOType.FERMI_ANNIHIL)],
+            coeffs)
+        H = qib.operator.FieldOperator([term])
+        self.assertTrue(H.is_hermitian())
         
-        terms = [qib.operator.FieldOperatorTerm(
-            [qib.operator.IFODesc(field,
-                np.random.choice((qib.operator.IFOType.FERMI_CREATE,
-                                  qib.operator.IFOType.FERMI_ANNIHIL))) for n in range(nops)],
-            qib.util.crandn(nops * (latt.nsites,))) for nops in range(4)]
-        H = qib.FieldOperator(terms)
         # encode Hamiltonian
         P = qib.transform.parity_encode_field_operator(H)
 
+        H_eig = np.sort_complex(np.linalg.eigvalsh(H.as_matrix().toarray()))
+        P_eig = np.sort_complex(np.linalg.eigvalsh(P.as_matrix().toarray()))
+
         # compare
-        self.assertTrue(np.allclose(np.linalg.eigvals(H.as_matrix().toarray()),
-                                    np.linalg.eigvals(P.as_matrix().toarray()),
-                                    rtol=1e-11,atol=1e-14))
-        #self.assertLess(sparse.linalg.norm(H.as_matrix() - P.as_matrix()), 1e-13)              # in matrix representation we use Jordan Wigner
+        self.assertTrue(np.allclose(H_eig, P_eig, rtol=1e-8,atol=1e-10))
+        #self.assertLess(sparse.linalg.norm(H.as_matrix() - P.as_matrix()), 1e-13)                  # as_matrix() uses Jordan-Wigner!
 
 
 if __name__ == "__main__":
